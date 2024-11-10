094ef90afa33ae7b1e46aca0e71026a6296aa9e1
nanaiwamoto <c5ijwtbwytn@gmail.com>
2024-11-10 15:28:58 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "items#index"
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :comments, only: :create 
-  end
-  resources :users, only: :show
+  resources :items, only: :index
 end
