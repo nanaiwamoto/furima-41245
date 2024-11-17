327179835a186a07dade38628abb4c2a4f9d0e4f
nanaiwamoto <c5ijwtbwytn@gmail.com>
2024-11-17 16:18:54 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 class ApplicationController < ActionController::Base
+  #ログインしていないユーザーをログインページの画面に促す
   before_action :authenticate_user!
 
   # DeviseヘルパーメソッドをViewで使用するための記述
   helper_method :current_user, :user_signed_in?, :user_session
 end
 
-class ItemsController < ApplicationController
-  def index
-    @items = Item.all
-  end
-end
+
