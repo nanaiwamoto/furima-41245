aedeb001a59f08ccbde652befb0306601b0cd9d7
nanaiwamoto <c5ijwtbwytn@gmail.com>
2024-11-22 19:20:50 +0900

    再起動

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_21_064748) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_22_033844) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,6 +52,19 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_21_064748) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "order_addresses", charset: "utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "shopping_region_id", null: false
+    t.string "city", null: false
+    t.string "street_address", null: false
+    t.string "building_name"
+    t.string "tel", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_addresses_on_order_id"
   end
 
   create_table "orders", charset: "utf8", force: :cascade do |t|
@@ -98,6 +111,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_21_064748) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "order_addresses", "orders"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
   add_foreign_key "shipping_addresses", "orders"
