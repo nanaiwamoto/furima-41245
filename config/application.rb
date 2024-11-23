7374701cb7182c02cb8685fe8113ce7ab038ca06
nanaiwamoto <c5ijwtbwytn@gmail.com>
2024-11-23 12:19:27 +0900

    en

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,7 @@ module Furima41245
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     #  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.default_locale = :ja
-    config.i18n.available_locales = [:ja, :en]
+    config.i18n.available_locales = [:en]
     config.i18n.enforce_available_locales = true
   end
 end
